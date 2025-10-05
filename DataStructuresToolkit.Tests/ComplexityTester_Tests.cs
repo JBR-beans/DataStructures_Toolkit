b01fc2e4694d03726d8dd423f16404f4fcbe77d4
RustyBeans <107262647+RustyBeans@users.noreply.github.com>
2025-10-04 17:50:56 -0700

    added ComplexityTester

---
+++ DataStructuresToolkit.Tests/ComplexityTester_Tests.cs
@@ -0,0 +1,68 @@
+﻿using DataStructuresToolkit;
+using NUnit.Framework;
+using System.Diagnostics;
+
+namespace DataStructuresToolkit.Tests
+{
+	public class ComplexityTester_Tests
+	{
+		private int[][] arrays = new int[3][];
+		private ComplexityTester tester = new ComplexityTester();
+
+		[SetUp]
+		public void Setup()
+		{
+			arrays[0] = new int[1000];
+			arrays[1] = new int[10000];
+			arrays[2] = new int[100000];
+		}
+
+		[Test]
+		public void Constant_Test()
+		{
+			var sw = new Stopwatch();
+			for (int i = 0; i < arrays.Length; i++)
+			{
+				int expectedValue = 0;
+				sw.Start();
+				int result = tester.RunConstantScenario(arrays[i]);
+				sw.Stop();
+				TestContext.WriteLine($"n={arrays[i].Length}: {sw.ElapsedMilliseconds} ms");
+				sw.Reset();
+				Assert.That(result, Is.EqualTo(expectedValue));
+			}
+		}
+
+		[Test]
+		public void Linear_Test()
+		{
+			var sw = new Stopwatch();
+			for (int i = 0; i < arrays.Length; i++)
+			{
+				int expectedValue = arrays[i].Length;
+				sw.Start();
+				int result = tester.RunLinearScenario(arrays[i]);
+				sw.Stop();
+				TestContext.WriteLine($"n={arrays[i].Length}: {sw.ElapsedMilliseconds} ms");
+				sw.Reset();
+				Assert.That(result, Is.EqualTo(expectedValue));
+			}
+		}
+
+		[Test]
+		public void Quadratic_Test()
+		{
+			var sw = new Stopwatch();
+			for (int i = 0; i < arrays.Length; i++)
+			{
+				long expectedValue = (long)arrays[i].Length * (long)arrays[i].Length;
+				sw.Start();
+				long result = tester.RunQuadraticScenario(arrays[i]);
+				sw.Stop();
+				TestContext.WriteLine($"n={arrays[i].Length}: {sw.ElapsedMilliseconds} ms");
+				sw.Reset();
+				Assert.That(result, Is.EqualTo(expectedValue));
+			}
+		}
+	}
+}
---
+++ DataStructuresToolkit/ComplexityTester.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace DataStructuresToolkit
+{
+	public class ComplexityTester
+	{
+
+		/// <summary>
+		/// This represents O(1).
+		/// </summary>
+		/// <param name="input">
+		/// Only the first element of the input will be returned.
+		/// </param>
+		/// <remarks>
+		/// The complexity is constant, not growing with input size.
+		/// </remarks>
+		public int RunConstantScenario(int[] input)
+		{
+			return input[0];
+		}
+
+		/// <summary>
+		/// This method runs in O(n) time.
+		/// </summary>
+		/// <param name="input">
+		/// The input size, which determines how many operations take place.
+		/// </param>
+		/// <remarks>
+		/// Time complexity increases linearly with the input size.
+		/// </remarks>
+		public int RunLinearScenario(int[] input)
+		{
+			int count = 0;
+			for (int i = 0; i < input.Length; i++)
+			{
+				count++;
+			}
+			return count;
+		}
+
+		/// <summary>
+		/// This represents O(n²).
+		/// </summary>
+		/// <param name="input">
+		/// The input will determine how many operations will take place.
+		/// </param>
+		/// <remarks>
+		/// The complexity grows proportionally to the square of the input size.
+		/// </remarks>
+		public long RunQuadraticScenario(int[] input)
+		{
+			long combinations = 0;
+			for(int i = 0; i < input.Length; i++)
+			{
+				for(int j = 0; j < input.Length; j++)
+				{
+					combinations++;
+				}
+			}
+			return combinations;
+		}
+	}
+}
